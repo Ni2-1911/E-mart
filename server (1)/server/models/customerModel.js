3851960d29185ed1d3b40fe0fecd77a019eb2908
Nitu Kumari <79151737+Ni2-1911@users.noreply.github.com>
2022-06-23 19:28:01 +0530

    Delete server (1)/server directory

--- server (1)/server/models/customerModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const customerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Customer = mongoose.model("customer", customerSchema);
-
-module.exports = Customer;
--- server (1)/server/routers/wishlistRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const WishList = require("../models/wishlistModel");
-
-// add the wishlist to the particular person's wishlist
-// app.post('/:userId', () => {
-    
-// })
-
-module.exports = router;
